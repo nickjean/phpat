b9cd148f27ec468fe0177cd3cab2075c5d467990
Nickerson <nickerson.jbaptiste@hotmail.com>
2015-12-17 13:24:58 -0500

    Ajout des fichiers php

--- view_parts/_main_menu.php
+++ view_parts/_main_menu.php
@@ -9,6 +9,8 @@
 
 <ul>
     <?php
-
+        foreach ($menu_data as $menu => $menu_url) {
+            echo "<li><a href='".$menu_url."'>$menu</a></li>";
+        }
     ?>
 </ul>
--- view_parts/_page_base.php
+++ view_parts/_page_base.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title><?= SITE_NAME; ?></title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css"/>
 </head>
 <body>
     <?php require_once '_header.php'; ?>
+    <?php require_once '_main_menu.php'; ?>
